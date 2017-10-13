0f7e82bfcdf62d8269f5fa62c098cfa3c8464c92
Chiranjeevi <jeevi146@gmail.com>
2017-10-12 23:07:55 -0400

    validation level 1

--- handler.js
+++ handler.js
@@ -1,16 +1,11 @@
 'use strict';
+const dispatch = require('./dispatch');
 
 module.exports.intents = (event, context, callback) => {
-  const response = {
-    statusCode: 200,
-    body: JSON.stringify({
-      message: 'Go Serverless v1.0! Your function executed successfully!',
-      input: event,
-    }),
-  };
-
-  callback(null, response);
-
-  // Use this code if you don't use the http event with the LAMBDA-PROXY integration
-  // callback(null, { message: 'Go Serverless v1.0! Your function executed successfully!', event });
+  try {
+    console.log(`event.bot.name=${event.bot.name}`);
+    dispatch(event, (response) => callback(null, response))
+  } catch (err) {
+    callback(err);
+  }
 };
